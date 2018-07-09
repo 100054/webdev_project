3a8033fda0eea55a0f2e1c8a19edfd45b02dffb8
100054 <31431484+100054@users.noreply.github.com>
2018-07-09 16:56:53 +0300

    Add files via upload

---
+++ About.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html/>
+<html>
+		
+<head>
+		<title>About Us</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="hero-image">
+        <a href="home.php"><button class="button"><i>Home</i></button></a>
+        <a href="offers.php" ><button class="button"><i>Offers</i></button></a>
+        <a href="about.php"><button class="button"><i>About</i></button></a>  
+        <a href="contact.php"><button class="button"><i>Contact Us</i></button></a>  
+ 		 <div class="hero-text">
+    		<h1 style="font-size:40px"> BlackRose Catering Services</h1>
+    		<p><h2 style="font-size:160%">Where tasteful creations begin </h2></p>
+    		<a href="order.php"><button>Place Order</button></a>
+  		</div>
+	</div>
+<h4>OUR SERVICES:</h4>
+<p>BlackRose Catering Services started with the passion for cooking by four friends which led them to starting a business for offering food services.
+<br/>
+<p>It all started back in the university when they used to cook and sell their foods to friends, students and lecturers in the university.<br>Many people loved their food.<br> Also they would sell their food during events around the university.
+
+<br/>
+<p>BlackRose Catering Services is known fo its best services to its customers.<br>
+We take orders for food,snacks and drinks<br>
+Booking to be in advance for conveniency<br>
+It is surely the best place to be.<br>
+Come in and enjoy!<br></br>
+
+	<a href="#" class="fa fa-phone"></a> +254702037288 or +254740804376 &nbsp;
+	<a href="#" class="fa fa-google"></a> blackrose@caterers.com &nbsp; &nbsp;
+	<a href="#" class="fa fa-twitter"></a> @ __blackrosecaterers_ &nbsp; &nbsp;
+	<a href="#" class="fa fa-snapchat-ghost"></a> @__blackrose_catering254  &nbsp;</br>
+	<a href="#" class="fa fa-facebook"></a> @__blackrose_caterers  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+	<a href="#" class="fa fa-instagram"></a> @__blackrosecaterers254   &nbsp; 
+	<a href="#" class="fa fa-pinterest"></a> @__blackrosecaterers254  &nbsp; 
+	<a href="#" class="fa fa-tumblr"></a> @__blackrose_caterers254
+
+	
+</body>
+</html>
